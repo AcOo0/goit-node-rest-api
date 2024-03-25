f315d73700ac6070c6c44343ce5e47026305516d
AcOo0 <acoo.012@gmail.com>
2024-03-26 00:31:14 +0200

    Update homework

---
+++ models/hooks.js
@@ -0,0 +1,15 @@
+export const handleSaveError = (error, data, next) => {
+  error.status = 400;
+  next();
+};
+
+export const setUpdateSetting = function (next) {
+  this.options.new = true;
+  this.options.runValidators = true;
+  next();
+};
+
+export const handleUpdateError = (error, data, next) => {
+  error.status = 404;
+  next();
+};
